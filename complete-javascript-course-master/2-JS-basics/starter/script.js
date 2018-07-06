4b86ba873af047bc3eb8c9919b7ecdc7129895dc
Matt Motes <mmotes@library.msstate.edu>
2018-07-06 09:19:15 -0500

    The Ternary Operator (or conditional operator) and switch Statements

--- complete-javascript-course-master/2-JS-basics/starter/script.js
+++ complete-javascript-course-master/2-JS-basics/starter/script.js
@@ -202,7 +202,7 @@ if (bmiMark > bmiJohn) {
 // && === AND
 // || === or
 // ! === not
-
+/*
 var firstName = 'John';
 var age = 25;
 
@@ -213,5 +213,62 @@ if (age < 13) {
 } else if (age >= 20 && age < 30) { // between 20 and 30 years === age
   console.log(firstName + ' is a young man.');
 } else {
+  console.log(firstName + ' is a man.');
+}
+*/
+
+/*******************************************************************************
+* The Ternary Operator (or conditional operator) and switch Statements
+*/
+
+var firstName = 'John';
+var age = 10;
+// Ternary operator
+age >= 18 ? console.log(firstName + ' drinks beer.')
+: console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+// the statement above replaces an if else statement (makes it cleaner)
+
+/*if (age >= 18) {
+  var drink ='beer';
+} else {
+  var drink ='juice';
+}*/
+
+// switch Statements
+// normal switch statements are in strings
+var job = 'instructor';
+switch (job) {
+  case 'teacher':
+  case 'instructor':
+    console.log(firstName + ' teaches kids how to code.');
+    break;
+  case 'driver':
+    console.log(firstName + ' drives an uber in Lisbon.');
+    break;
+  case 'designer':
+    console.log(firstName + ' designs stuff.');
+    break;
+  default:
+  console.log(firstName + ' does something else.');
+}
+
+
+// switch statement using a boolean logic statement
+// there are some cases where you can do switch statments with variable conditions
+switch (true){
+  case age < 13:
+    console.log(firstName + ' is a boy.');
+    break;
+  case age >= 13 && age < 20:
+    console.log(firstName + ' is a teenager.');
+    break;
+  case age >= 20 && age < 30:
+    console.log(firstName + ' is a young man.');
+    break;
+  default:
   console.log(firstName + ' is a man.');
 }
