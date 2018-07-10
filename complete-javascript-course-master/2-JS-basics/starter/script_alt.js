1c60003202c4107d6d56b9eee799c2c9d604847d
Matt Motes <mmotes@library.msstate.edu>
2018-07-10 13:48:58 -0500

    function practice

---
+++ complete-javascript-course-master/2-JS-basics/starter/script_alt.js
@@ -0,0 +1,23 @@
+//piece of code that does one or more actions - function
+// don't repeat yourself DRY
+
+/*function go(name, age){
+  alert(name);
+  alert(age);
+}
+
+go('Will', 34);
+go('Matt', 29);
+
+
+function add(first, second) {
+  return first + second;
+}
+var sum = add (1, 2);
+alert(sum);
+*/
+
+function add (first, second){
+  return first + second;
+}
+alert( add(1, 2) );
